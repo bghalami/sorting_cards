00edf109f12d568f98022144e5b84f6694612684
bghalami <benjaminghalami@gmail.com>
2018-07-02 20:25:40 -0600

    DRYing up my deck and round classes/tests

--- test/round_test.rb
+++ test/round_test.rb
@@ -13,24 +13,6 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, round
   end
 
-  def test_deck
-    card_1 = Card.new("2","Spades")
-    card_2 = Card.new("2","Hearts")
-    card_3 = Card.new("6","Clubs")
-    deck = Deck.new([card_1,card_2,card_3])
-    round = Round.new(deck)
-    assert_equal deck, round.deck
-  end
-
-  def test_current_card
-    card_1 = Card.new("2","Spades")
-    card_2 = Card.new("2","Hearts")
-    card_3 = Card.new("6","Clubs")
-    deck = Deck.new([card_1,card_2,card_3])
-    round = Round.new(deck)
-    assert_equal card_1, round.current_card
-  end
-
   def test_record_guess_correct
     card_1 = Card.new("2","Spades")
     card_2 = Card.new("2","Hearts")
@@ -53,15 +35,6 @@ class RoundTest < Minitest::Test
     assert_equal 0, round.number_correct
     assert_equal card_1, round.current_card
     assert_equal 1, round.guesses.length
-  end
-
-  def test_guesses
-    card_1 = Card.new("2","Spades")
-    card_2 = Card.new("2","Hearts")
-    card_3 = Card.new("6","Clubs")
-    deck = Deck.new([card_1,card_2,card_3])
-    round = Round.new(deck)
-    assert_equal 0, round.guesses.length
   end
 
   def test_number_correct
