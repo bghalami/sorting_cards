437f87715b7cb308432956c9eca3da444c86f99f
bghalami <benjaminghalami@gmail.com>
2018-07-04 19:27:02 -0600

    Moved comparing value to card initialize

--- lib/card.rb
+++ lib/card.rb
@@ -5,5 +5,24 @@ require "pry"
    def initialize(value, suit)
      @value = value
      @suit = suit
+     lookup = { "2" => 20,
+                "3" => 30,
+                "4" => 40,
+                "5" => 50,
+                "6" => 60,
+                "7" => 70,
+                "8" => 80,
+                "9" => 90,
+                "10" => 100,
+                "Jack" => 110,
+                "Queen" => 120,
+                "King" => 130,
+                "Ace" => 140,
+                    }
+     suit_value = {"Clubs" => 1,
+                    "Diamonds" => 2,
+                    "Hearts" => 3,
+                    "Spades" => 4}
+    @comparing_value = suit_value[suit] + lookup[value]
    end
  end
