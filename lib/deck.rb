57620ab5d358374b0b906f454b5ede3818cc6cf6
bghalami <benjaminghalami@gmail.com>
2018-07-04 20:29:52 -0600

    finished adding merge sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,6 +6,29 @@ class Deck
     @cards = cards
   end
 
+  def merge_sort(sort_array = @cards)
+    if sort_array.length <= 1
+      sort_array
+    else
+      mid = (sort_array.length / 2).floor
+      left = merge_sort(sort_array[0..mid - 1])
+      right = merge_sort(sort_array[mid..sort_array.length])
+      merge(left, right)
+    end
+  end
+
+  def merge(left, right)
+    if left.empty?
+      right
+    elsif right.empty?
+      left
+    elsif left[0].comparing_value < right[0].comparing_value
+        [left[0]] + merge(left[1..left.length],right)
+    else
+      [right[0]] + merge(left, right[1..right.length])
+    end
+  end
+
   def sort
     swapped = true
     while swapped do
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -21,6 +21,16 @@ class DeckTest < Minitest::Test
     assert_equal [card_2, card_1, card_3, card_5, card_4], deck.sort
   end
 
+  def test_merge_sort
+    card_1 = Card.new("2","Spades")
+    card_2 = Card.new("2","Hearts")
+    card_3 = Card.new("6","Clubs")
+    card_4 = Card.new("Ace","Spades")
+    card_5 = Card.new("Jack","Hearts")
+    deck = Deck.new([card_1,card_2,card_3,card_4,card_5])
+    assert_equal [card_2, card_1, card_3, card_5, card_4], deck.merge_sort
+  end
+
   def test_count
     card_1 = Card.new("2","Spades")
     card_2 = Card.new("2","Hearts")
